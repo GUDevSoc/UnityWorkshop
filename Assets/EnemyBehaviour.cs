9800d16881c37e74030a30d799741bca49e8aab9
PiotrusWatson <piotruswatson@gmail.com>
2017-10-02 11:55:21 +0100

    untested attempt at flipping direction oops lol

--- Assets/EnemyBehaviour.cs
+++ Assets/EnemyBehaviour.cs
@@ -6,23 +6,43 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour {
 
 	Rigidbody2D rb2D;
-	Transform R_Edge, L_Edge;
 
 	private int groundmask;
+	private bool grounded, onEdge;
+	Transform EdgeCheck, GroundCheck;
+
+	[SerializeField]
+	float speed;
 
 	// Use this for initialization
 	void Awake () {
 		groundmask = 1 << LayerMask.NameToLayer ("Ground");
 		rb2D = GetComponent<Rigidbody2D> ();
 
-		R_Edge = transform.Find ("EdgeCheck_R");
-		L_Edge = transform.Find ("EdgeCheck_L");
+		EdgeCheck = transform.Find ("EdgeCheck");
+		GroundCheck = transform.Find ("GroundCheck");
 
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		grounded = Physics2D.OverlapCircle (GroundCheck.position,
+			GroundCheck.GetComponent<CircleCollider2D> ().radius, 
+			groundmask);
+
+		onEdge = Physics2D.OverlapCircle (EdgeCheck.position,
+			EdgeCheck.GetComponent<CircleCollider2D> ().radius, 
+			groundmask);
+
+
+		if (grounded && !onEdge)
+			transform.localScale = new Vector3 (transform.localScale.x * -1, 
+				transform.localScale.y, 
+				transform.localScale.z);
+
+
+
 		
 	}
 
