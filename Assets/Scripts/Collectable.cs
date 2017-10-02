a9d9fe61cb7e20e01249179d75cab7d59308f41f
KinRyuuXIV <tom.f.whitehouse@btinternet.com>
2017-10-02 15:16:39 +0100

    Coins!

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D collision){
+
+		if (collision.gameObject.transform.name == "Player"){
+			CollectableBehaviour (collision);
+			Destroy (gameObject);
+		}
+			
+	}
+
+	void CollectableBehaviour(Collision2D collision){
+		collision.gameObject.GetComponent<ScoreTracker> ().score++;
+	}
+}
